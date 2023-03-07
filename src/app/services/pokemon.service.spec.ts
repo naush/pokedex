038e5370ffd268a993ba37db91d2bb041aeaed62
Li-Hsuan Lung <lihsuanlung@gmail.com>
2023-03-06 20:43:11 -0500

    Add error response test cases

--- src/app/services/pokemon.service.spec.ts
+++ src/app/services/pokemon.service.spec.ts
@@ -45,12 +45,29 @@ describe('PokemonService', () => {
 
       handler.expectNone(`${service.baseUrl}/pokedex/${service.region}`);
     });
+
+    it('handles error', (done: DoneFn) => {
+      const error = { message: 'Service is down' }
+
+      service.all().subscribe(
+        (response) => { },
+        (errorResponse) => {
+          expect(errorResponse.error).toBe(error);
+          expect(errorResponse.status).toBe(504);
+          done();
+        }
+      );
+
+      const req = handler.expectOne(`${service.baseUrl}/pokedex/${service.region}`);
+
+      req.flush(error, { status: 504, statusText: 'Service Unavailable' });
+    });
   });
 
   describe('get', () => {
-    it('responds with one pokemon', (done: DoneFn) => {
-      const pokemonNumber = 1;
+    const pokemonNumber = 1;
 
+    it('responds with one pokemon', (done: DoneFn) => {
       service.get(pokemonNumber).subscribe((response) => {
         expect(response).toEqual(new Pokemon(oneResponse));
 
@@ -60,6 +77,23 @@ describe('PokemonService', () => {
       const req = handler.expectOne(`${service.baseUrl}/pokemon/${pokemonNumber}`);
 
       req.flush(oneResponse);
+    });
+
+    it('handles error', (done: DoneFn) => {
+      const error = { message: 'Service is down' }
+
+      service.get(pokemonNumber).subscribe(
+        (response) => { },
+        (errorResponse) => {
+          expect(errorResponse.error).toBe(error);
+          expect(errorResponse.status).toBe(504);
+          done();
+        }
+      );
+
+      const req = handler.expectOne(`${service.baseUrl}/pokemon/${pokemonNumber}`);
+
+      req.flush(error, { status: 504, statusText: 'Service Unavailable' });
     });
   });
 });
